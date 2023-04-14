8a879456301e8fb573a5d5c76ea95c9132382422
sameh <sameh.m22@outlook.com>
2023-04-14 14:13:20 +0200

    Day_1 project

---
+++ Day_1/main.py
@@ -0,0 +1,11 @@
+#band name generator program
+
+# welcome screen to Band name generator
+print("welcome to Band Name generator ")
+
+# ask user for favorite city and pet
+city = input("Please enter you favorite city: ")
+pet = input("Please enter your favorite pet: ")
+
+# print Band name
+print(f"your Band name is '{city} {pet}'")
