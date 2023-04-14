5f64f2a72bbd98357211f9b894f050db1d6e2961
sameh <sameh.m22@outlook.com>
2023-04-14 16:33:16 +0200

    age calculator

---
+++ Day_2/how_long_do_live/main.py
@@ -0,0 +1,15 @@
+# calculate your age in months, weeks, Days
+from datetime import date
+
+
+# get user_age
+year = int(input("What year were you born?\n"))
+
+# get the current year and subtract it form the year the user born in
+age = date.today().year - year
+
+# print the results
+print(f"your age in years is {age}\n"
+      f"your age in months is {age * 12} months\n"
+      f"your age in Weeks is {age * 52} Weeks\n"
+      f"your age in Days is {age * 365} Days")
