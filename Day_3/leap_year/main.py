4ac501eddc2dfd22fb4848289d4381a4d03ec1c4
sameh <sameh.m22@outlook.com>
2023-04-16 10:50:33 +0200

    leap year project modification

--- Day_3/leap_year/main.py
+++ Day_3/leap_year/main.py
@@ -12,6 +12,10 @@ def leap_year(year):
         if year % 100 == 0:
             if year % 400 == 0:
                 return True
+            else:
+                return False
+        else:
+            return True
     else:
         return False
 
