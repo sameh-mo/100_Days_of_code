413f744ebfed7ce79f4e63a8757fcce4f28e91ec
sameh <sameh.m22@outlook.com>
2023-04-16 09:52:16 +0200

    Day_2 and Day_3 projects

---
+++ Day_3/leap_year/main.py
@@ -0,0 +1,24 @@
+# leap year detector program
+
+#welcome screen
+print("welcome to leap year detector program")
+
+# prompt to get the year
+year = int(input("Enter the year please: "))
+
+# leap year checker function
+def leap_year(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+    else:
+        return False
+
+
+# message
+
+if leap_year(year):
+    print(f"{year} is a leap year")
+else:
+    print(f"{year} is not a leap year ")
